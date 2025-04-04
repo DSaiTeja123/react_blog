5a3f753dec8e5a0001c529063bf5fd7c6a650e1a
DSaiTeja123 <22341a0539@gmrit.edu.in>
2025-04-04 22:46:34 +0530

    final commit

--- src/components/Header/LogoutBtn.jsx
+++ src/components/Header/LogoutBtn.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import authService from '../../appwrite/auth';
 import { logout } from '../../store/authSlice';
 
 function LogoutBtn() {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const logoutHandler = () => {
     authService.logout().then(() => {
       dispatch(logout());
+      navigate('/'); // Navigate to homepage after logout
     });
   };
 
